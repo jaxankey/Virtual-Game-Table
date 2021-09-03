052629a621d1d89e25b3e1758d929861163ec546
Jack <jack.sankey@gmail.com>
2021-09-03 14:05:08 -0400

    checkers

--- games/checkers/game.js
+++ games/checkers/game.js
@@ -29,57 +29,48 @@ VGT.images = {
     // Base path relative to private/game/, game/, or common/ folders (listed in search order)
     root: 'images',
 
+    // Keys and paths relative to the root directory. Keys are used to create pieces below.
     paths: {
 
       // Required for player hands
       'hand': 'hands/hand.png',
       'fist': 'hands/fist.png',
 
-      'board' : 'chess/board.png',
-      'pawn'  : 'chess/pawn.png',
-      'rook'  : 'chess/rook.png',
-      'knight': 'chess/knight.png',
-      'bishop': 'chess/bishop.png',
-      'king'  : 'chess/king.png',
-      'queen' : 'chess/queen.png',
-
+      'board'         : 'checkers/board.png',
+      'checker_black' : 'checkers/checker_black.png',
+      'king_black'    : 'checkers/king_black.png',
+      'checker_red'   : 'checkers/checker_red.png',
+      'king_red'      : 'checkers/king_red.png',
+    
     } // End of paths
 }
 
 // Create the Game instance (also stores itself in VGT.game)
 game = new VGT.Game({
-  name           : 'Chess',        // Game name
-  nameplate_xyrs : [480, 0, 0, 1], // Spawn point for new nameplates
-  background_color: 0xBBBBBB,
+  name             : 'Checkers',     // Game name
+  nameplate_xyrs   : [480, 0, 0, 1], // Spawn point for new nameplates
+  background_color : 0xBBBBBB,       // Background color of the tabletop
 });
 
 
 
 ///////////////////////////// GAME BOARD
 var settings = {
-  
-  layer:  0,   // Number of layer in which this piece lives
-  teams:  [],  // Which teams can grab / move this piece
 
-  // List of lists of image keys to use for each of the piece's internal layers,
-  // or a list of image keys, or an image key.
-  images: 'board', 
+  layer:  0,       // Number of layer in which this piece lives
+  teams:  [],      // Which teams can grab / move this piece
+  images: 'board', // Images specifier (can be a list or list of lists)
   
-  // List of snap object; in this case a single square grid for pieces
-  snaps: [
-    { // Small buildings grid 
+  // List of snap objects; in this case a single square grid for pieces
+  snaps: [{ 
       type: VGT.SnapGrid, // class used to create this snap
       groups: ['pieces'], // list of groups that will snap
-      x0: -224,              // Origin of grid, x-coordinate
-      y0: -224,              // Origin of grid, y-coordinate                   
-      ax: 64,             // Basis vector 'a', x-coordinate
-      ay: 0,              // Basis vector 'a', y-coordinate
-      bx: 0,              // Basis vector 'b', x-coordinate
-      by: 64,             // Basis vector 'b', y-coordinate
+      x0: -224, y0: -224, // Origin of the grid                   
+      ax: 64,   ay: 0,    // Basis vector 'a'
+      bx: 0,    by: 64,   // Basis vector 'b'
       r : 0,              // Rotation when snapped
       boundary: [-256,-256, 256,-256, 256,256, -256,256], // Polygon boundary points
-    },
-  ], // End of snaps
+    }], 
 
 } // End of settings
 var board = game.add_piece(settings)
@@ -91,28 +82,13 @@ var settings = {
   layer:  1,              // Layer of the piece
   groups: ['pieces'],     // Groups to which this piece belongs
   rotate_with_view: true, // Piece always keeps its orientation relative to the screen when we rotate the board
-  width:  64,
-  height: 64,
+  shape: 'circle',        // Shape of hitbox for clicking & for drawing the selection
+  width: 64, height: 64,  // Override the image width and height for the hitbox
 }
 
-// White pieces
-settings.tint = 0xFAF2E1
-var w_pawns   = game.add_pieces(8, settings, 'pawn')
-var w_rooks   = game.add_pieces(2, settings, 'rook')
-var w_knights = game.add_pieces(2, settings, 'knight')
-var w_bishops = game.add_pieces(2, settings, 'bishop')
-var w_queen   = game.add_piece(    settings, 'queen')
-var w_king    = game.add_piece(    settings, 'king')
-
-// Black pieces
-settings.tint = 0xCC4422
-var b_pawns   = game.add_pieces(8, settings, 'pawn')
-var b_rooks   = game.add_pieces(2, settings, 'rook')
-var b_knights = game.add_pieces(2, settings, 'knight')
-var b_bishops = game.add_pieces(2, settings, 'bishop')
-var b_queen   = game.add_piece(    settings, 'queen')
-var b_king    = game.add_piece(    settings, 'king')
-
+// Add the pieces
+var reds   = game.add_pieces(12, settings, ['checker_red'  , 'king_red'  ])
+var blacks = game.add_pieces(12, settings, ['checker_black', 'king_black'])
 
 //////////////////////////////////// NEW GAME SETUP
 
