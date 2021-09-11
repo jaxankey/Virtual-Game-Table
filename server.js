3e4ee8ef417e8625f63d9fd77b5eebba03294bed
Jack <jack.sankey@gmail.com>
2021-09-10 23:45:44 -0400

    hella

--- server.js
+++ server.js
@@ -214,7 +214,7 @@ var last_id     = 1;  // Last assigned id; incremented with each client
 // Names for new players
 var pre_names = ['William T.', 'Billy D.', 'Johnny', 'Susan B.', 'Karen', 'Mr.', 'Ms.', 'Mrs.', 'Dr.', 'Prof.', 'M.', 'Mme.', 'Mlle.']
 
-var first_names = ['Pants', 'Silly', 'Fancy', 'Dirt', 'Goofy',
+var first_names = ['Pants', 'Silly', 'Fancy', 'Dirt', 'Goofy', 'Hella',
                    'Trash', 'No', 'Awful', 'Nono', 'Juicy'];
 var last_names  = ['tastic', 'cakes', 'pants', 'face', 'juice', 
                    'bag', 'hole', 'friends', 'skillet', 'person', 'billy',
