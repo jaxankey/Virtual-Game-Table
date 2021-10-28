b070293ad176ea05a7064a36b3519b0f61fbdd44
Jack <jack.sankey@gmail.com>
2021-10-28 10:03:03 -0400

    Update engine.js

--- common/engine.js
+++ common/engine.js
@@ -232,7 +232,7 @@ class _Html {
 
         // link variable should be properly formed
         var l = s[i];
-        if(l.indexOf('@')  >= 0) l = 'mailto:'+s[i];
+        if     (l.indexOf('@')  >= 0) l = 'mailto:'+s[i];
         else if(l.indexOf('://') < 0) l = 'http://'+s[i];
         s[i] = '<a href="'+l+'" target="new">'+s[i]+'</a>';
       } 
