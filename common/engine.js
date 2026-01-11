61b26b934a47a793685fbc1c3219ca13d5573e7c
Jack Sankey <jack.sankey@gmail.com>
2026-01-11 18:35:38 -0500

    Update engine.js

--- common/engine.js
+++ common/engine.js
@@ -1646,7 +1646,9 @@ class _Interaction {
   }
 
   // Pointer touches the underlying surface.
-  onpointerdown(e) {
+  onpointerdown(e) { VGT.log('onpointerdown()', e.button, this);
+
+    
 
     e.preventDefault();
     this.last_pointerdown = e;
@@ -1687,8 +1689,6 @@ class _Interaction {
 
     // Find a thing under the pointer if there is one.
     var thing = VGT.game.get_top_thing_at(v.x,v.y);
-
-    VGT.log('onpointerdown()', [e.clientX, e.clientY], '->', v, e.button, this.tabletop_xd, this.tabletop_yd, 'thing1', thing,'thing2',this);
 
     // If it's not null and we can grab it
     if(thing != null && thing.is_grabbable_by_me()) {
