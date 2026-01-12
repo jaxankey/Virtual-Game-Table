46c64cbbfd5b639a32be84d87e50c8830236192d
Jack Sankey <jack.sankey@gmail.com>
2026-01-11 19:25:56 -0500

    test

--- games/poker/game.js
+++ games/poker/game.js
@@ -634,7 +634,7 @@ VGT.interaction.after_ondblclick = function(e) {
   // First selected piece
   var p = ps[Object.keys(ps)[0]]
 
-  console.log('PANTS', ps, p._n)
+  console.log('PANTS', p, p.get_image_index())
   // Bar index
   //var n = bars.indexOf(p);
 
